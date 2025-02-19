84e730a989cdb85df3eff4697ce76d26391e0108
jabeena <jabina123@gmail.com>
2025-02-19 19:19:20 +0300

    Fixed invalid href in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ cart, toggleCart }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
-      <a className="navbar-brand fw-bold" href="#">Fake Store</a>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-          <li className="nav-item"><a className="nav-link" href="#">About</a></li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        {/* Use Link instead of <a href="/"> */}
+        <Link className="navbar-brand" to="/">Home</Link>
+
+        <button className="btn btn-primary" onClick={toggleCart}>
+          Cart ({cart.length})
+        </button>
       </div>
-      <button className="btn btn-primary position-relative" onClick={toggleCart}>
-         Cart 
-        {cart.length > 0 && (
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {cart.length}
-          </span>
-        )}
-      </button>
     </nav>
   );
 };
